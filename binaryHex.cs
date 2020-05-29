46bb5d52f38e6a3ef70fbb31b23c56ce654bf36f
leeyoungsup <58593829+Leeyoungsup@users.noreply.github.com>
2020-05-29 18:19:34 +0900

    part1_2

---
+++ binaryHex.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace development_part1
+{
+    class binaryHex
+    {
+        static void Main(string[] args)
+        {
+            //byte[1바이트0~255], short[2바이트], int[4바이트], long[8바이트]
+            //10진수
+            //01 02 03 04 05 06 07 08 09 10 11...
+            //2진수
+            //0~1
+            //0b01 0b10 0b11
+            //16진수
+            //00~09 0A 0B 0C 0D 0E 0F
+            //0x01~0x09 0x0A 0x0B 0x0C 0x0D 0x0E 0x0F
+            //Console.WriteLine("Hello Nunber! {0}",d);
+           
+        }
+    }
+}
