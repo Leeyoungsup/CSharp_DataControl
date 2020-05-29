cc2aa3f8d0fdc001bdb7fccba587b3eb118718ac
leeyoungsup <58593829+Leeyoungsup@users.noreply.github.com>
2020-05-29 14:58:18 +0900

    part1_1

---
+++ interger_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace development_part1
+{
+    class interger_form
+    {
+        static void Main(string[] args)
+        {
+            //byte[1바이트0~255], short[2바이트], int[4바이트], long[8바이트]
+            int a;
+            a = 100;
+            short b = 200;
+            long id = 112301203;
+            byte d = 0;
+            d--;
+            
+            Console.WriteLine("Hello Nunber! {0}",d);
+        }
+    }
+}
